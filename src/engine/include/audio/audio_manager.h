31820a6d59d10ca6c6a9e9718df60f46b5e98652
Benjamin Russell <benjamin.sheldon.russell@gmail.com>
2026-01-11 16:32:42 -0500

    Fix headers in audio_manager.h and finalize spatial audio tasks

--- src/engine/include/audio/audio_manager.h
+++ src/engine/include/audio/audio_manager.h
@@ -4,11 +4,11 @@
 #ifndef AUDIO_MANAGER_H
 #define AUDIO_MANAGER_H
 
-#include "include/audio/ambient_system.h"
-#include "include/audio/audio_effects.h"
-#include "include/audio/audio_system.h"
-#include "include/audio/music_system.h"
-#include "include/physics/block_physics.h"
+#include "audio/ambient_system.h"
+#include "audio/audio_effects.h"
+#include "audio/audio_system.h"
+#include "audio/music_system.h"
+#include "physics/block_physics.h"
 #include <common.h>
 #include <math/vec3.h>
 
