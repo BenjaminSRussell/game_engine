0583b4cae5de4e48e073e4fa488c28e2dbd918e2
Benjamin Russell <benjamin.sheldon.russell@gmail.com>
2026-01-13 16:52:46 -0500

    Style: Add proper header to rendering_tools_stubs.c

--- src/frontend/CVoxelForge/rendering_tools_stubs.c
+++ src/frontend/CVoxelForge/rendering_tools_stubs.c
@@ -1,3 +1,10 @@
+/**
+ * @file rendering_tools_stubs.c
+ * @brief Frontend Implementation
+ * @description Frontend system implementation
+ * @date 2026-01-13
+ */
+
 // swift_bridge_stubs_part4.c
 // Additional stub implementations for Swift bridge functions - Part 4
 
