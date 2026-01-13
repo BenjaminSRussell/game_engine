7a47f3d4fc17ab5467092b47f225a80007a08ba9
Benjamin Russell <benjamin.sheldon.russell@gmail.com>
2026-01-13 16:51:23 -0500

    Style: Add proper header to hot_reload.c

---
+++ src/engine/core/legacy/hot_reload.c
@@ -0,0 +1,9 @@
+/**
+ * @file $(basename "$file" .c).c
+ * @brief Core Implementation
+ * @description Core system implementation
+ * @date 2026-01-13
+ * @note Currently disabled - placeholder implementation
+ */
+
+// Disabled
