8882440c2853bad9499c61870f293f6b517069af
Benjamin Russell <benjamin.sheldon.russell@gmail.com>
2026-01-13 16:53:02 -0500

    Style: Add proper header to run_tests.c

--- src/tests/run_tests.c
+++ src/tests/run_tests.c
@@ -1,3 +1,10 @@
+/**
+ * @file run_tests.c
+ * @brief Test Implementation
+ * @description Test system implementation
+ * @date 2026-01-13
+ */
+
 // Test Runner
 // Main entry point for running all tests
 
