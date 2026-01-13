5f8cf556d5b8a4ed29a4dc998dead05ca1f0a065
Benjamin Russell <benjamin.sheldon.russell@gmail.com>
2026-01-13 16:48:34 -0500

    Style: Add proper header to ui_renderer.c

--- src/ui/ui_renderer.c
+++ src/ui/ui_renderer.c
@@ -1,1 +1,9 @@
+/**
+ * @file ui_renderer.c
+ * @brief UI Renderer Implementation
+ * @description Rendering system for UI components
+ * @date 2026-01-13
+ * @note Currently disabled - placeholder implementation
+ */
+
 // Disabled
