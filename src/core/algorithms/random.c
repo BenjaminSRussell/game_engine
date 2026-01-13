7b332a79646d15521cb187502b833a2e524faf3d
Benjamin Russell <benjamin.sheldon.russell@gmail.com>
2026-01-13 16:51:23 -0500

    Style: Add proper header to random.c

--- src/core/algorithms/random.c
+++ src/core/algorithms/random.c
@@ -1,3 +1,10 @@
+/**
+ * @file random.c
+ * @brief Core Implementation
+ * @description Core system implementation
+ * @date 2026-01-13
+ */
+
 /*
  * random.c
  * Random number generation algorithms
