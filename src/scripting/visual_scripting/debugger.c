aa0e94de1f0049b9d41a8b611e9c019205052fba
Benjamin Russell <benjamin.sheldon.russell@gmail.com>
2026-01-13 16:51:59 -0500

    Style: Add proper header to debugger.c

--- src/scripting/visual_scripting/debugger.c
+++ src/scripting/visual_scripting/debugger.c
@@ -1,1 +1,9 @@
+/**
+ * @file $(basename "$file" .c).c
+ * @brief Scripting Implementation
+ * @description Scripting system implementation
+ * @date 2026-01-13
+ * @note Currently disabled - placeholder implementation
+ */
+
 // Disabled
