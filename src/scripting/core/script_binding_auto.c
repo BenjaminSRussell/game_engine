62386a0f5fca7ca22bd5b2fa6fb91ec3a95ce3a8
Benjamin Russell <benjamin.sheldon.russell@gmail.com>
2026-01-13 16:51:14 -0500

    Style: Add proper header to script_binding_auto.c

--- src/scripting/core/script_binding_auto.c
+++ src/scripting/core/script_binding_auto.c
@@ -1,1 +1,9 @@
+/**
+ * @file $(basename "$file" .c).c
+ * @brief Network/Scripting Implementation
+ * @description Network and scripting component implementation
+ * @date 2026-01-13
+ * @note Currently disabled - placeholder implementation
+ */
+
 // Disabled
