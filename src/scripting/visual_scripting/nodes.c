e5ccf34f81b67c37c72d7a434daf8ea45394e242
Benjamin Russell <benjamin.sheldon.russell@gmail.com>
2026-01-13 16:51:56 -0500

    Style: Add proper header to nodes.c

--- src/scripting/visual_scripting/nodes.c
+++ src/scripting/visual_scripting/nodes.c
@@ -1,1 +1,9 @@
+/**
+ * @file nodes.c
+ * @brief Visual Scripting Nodes Implementation
+ * @description Visual scripting node system
+ * @date 2026-01-13
+ * @note Currently disabled - placeholder implementation
+ */
+
 // Disabled
